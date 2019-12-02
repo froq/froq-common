32ac1d4dbdbf1b213ba54b9c234e56080703aad0
Kerem Güneş <k-gun@mail.com>
2019-12-02 22:57:05 +0300

    traits.SingletonTrait: refactor [make for one class].

--- src/traits/SingletonTrait.php
+++ src/traits/SingletonTrait.php
@@ -38,44 +38,20 @@ use ReflectionClass;
 trait SingletonTrait
 {
     /**
-     * Instances.
-     * @var array<object>
+     * Instance.
+     * @var self
      */
-    private static array $__instances = [];
-
-    /**
-     * Forbids.
-     * @note Do not define '__clone' or '__construct' methods as public if you want a single
-     * sub-object. These methods ain't final here, so this trait could be used a base object
-     * for its all sub-objects.
-     */
-    private function __construct() {}
-    private function __clone() {}
+    private static $instance;
 
     /**
      * Init.
      * @param  ... $arguments
-     * @return object
+     * @return self
      */
-    public static final function init(...$arguments): object
+    public static final function init(...$arguments): self
     {
-        $className = static::class;
-
-        if (empty(self::$__instances[$className])) {
-            // Init object without constructor and call constructor with initial arguments to
-            // prevent error: "Access to non-public constructor of class ...". newInstance()
-            // nor newInstanceArgs() cannot do that.
-            $reflection = new ReflectionClass($className);
-            $classInstance = $reflection->newInstanceWithoutConstructor();
-
-            // prepare constructor to call
-            $constructor = $reflection->getConstructor();
-            $constructor->setAccessible(true);
-            $constructor->invokeArgs($classInstance, $arguments);
-
-            self::$__instances[$className] = $classInstance;
-        }
-
-        return self::$__instances[$className];
+        return self::$instance ? self::$instance : (
+               self::$instance = new self(...$arguments)
+        );
     }
 }
