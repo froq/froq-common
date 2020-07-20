67fc5409b4ed60fca58cc3f0db2115faee8f2075
Kerem Güneş <qeremy@gmail.com>
2020-07-20 23:04:53 +0300

    traits.ThrowableTrait: optimize toString() [extensions issue].

--- src/traits/ThrowableTrait.php
+++ src/traits/ThrowableTrait.php
@@ -135,8 +135,8 @@ trait ThrowableTrait
             $class = str_replace('\\', '.', $class);
 
             // Change dotable stuffs and remove php extensions.
-            $message = preg_replace('~(\w)(?:\\\|::|->)(\w)~',
-                '\1.\2', $message);
+            $message = preg_replace(['~(\w)(?:\\\|::|->)(\w)~', '~\.php~'],
+                ['\1.\2', ''], $message);
             $messageTrace = preg_replace_callback('~(?:\.php[(]|(?:\\\|::|->))~',
                 fn($m) => ($m[0] == '.php(') ? '(' : '.', $messageTrace);
         }
