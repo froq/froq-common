6193859eac04c2b0a5b0ef82f4a21bbf6e087f1e
Kerem Güneş <qeremy@gmail.com>
2020-12-13 22:34:20 +0300

    Add traits.DataTrait.

---
+++ src/traits/DataTrait.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * Copyright (c) 2015 · Kerem Güneş
+ * Apache License 2.0 <https://opensource.org/licenses/apache-2.0>
+ */
+declare(strict_types=1);
+
+namespace froq\common\traits;
+
+/**
+ * Data Trait.
+ *
+ * Represents a trait which carries `$data` property and is able to set/get actions.
+ *
+ * @package froq\common\traits
+ * @object  froq\common\traits\DataTrait
+ * @author  Kerem Güneş <k-gun@mail.com>
+ * @since   5.0
+ */
+trait DataTrait
+{
+    /** @var array $data */
+    protected array $data;
+
+    /**
+     * Set data property.
+     *
+     * @param  array $data
+     * @return self
+     */
+    public function setData(array $data): self
+    {
+        $this->data = $data;
+
+        return $this;
+    }
+
+    /**
+     * Get data property.
+     *
+     * @return array|null
+     */
+    public function getData(): array|null
+    {
+        return $this->data ?? null;
+    }
+}
