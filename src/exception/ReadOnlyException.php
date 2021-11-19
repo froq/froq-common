a9bd72bb557d124dc77652805b9f3846f8daf82c
Kerem Güneş <qeremy@gmail.com>
2021-11-19 18:27:00 +0300

    * exception: add AccessException,ReadOnlyException.

---
+++ src/exception/ReadOnlyException.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Copyright (c) 2015 · Kerem Güneş
+ * Apache License 2.0 · http://github.com/froq/froq-common
+ */
+declare(strict_types=1);
+
+namespace froq\common\exception;
+
+use froq\common\exception\RuntimeException;
+
+/**
+ * Read-Only Exception.
+ *
+ * @package froq\common\exception
+ * @object  froq\common\exception\ReadOnlyException
+ * @author  Kerem Güneş
+ * @since   5.5
+ */
+class ReadOnlyException extends RuntimeException
+{}
