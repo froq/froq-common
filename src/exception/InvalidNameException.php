d08d3a73799e3144068719c95a7aac2f83e57f35
Kerem Güneş <qeremy@gmail.com>
2021-12-10 20:05:33 +0300

    * exception: add InvalidNameException.

---
+++ src/exception/InvalidNameException.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Copyright (c) 2015 · Kerem Güneş
+ * Apache License 2.0 · http://github.com/froq/froq-common
+ */
+declare(strict_types=1);
+
+namespace froq\common\exception;
+
+use froq\common\exception\LogicException;
+
+/**
+ * Invalid Name Exception.
+ *
+ * @package froq\common\exception
+ * @object  froq\common\exception\InvalidNameException
+ * @author  Kerem Güneş
+ * @since   5.9
+ */
+class InvalidNameException extends LogicException
+{}
