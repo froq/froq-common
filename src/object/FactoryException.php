673ae49ce71c0e2df46e6a386c80359e8eada0c5
Kerem Güneş <qeremy@gmail.com>
2022-02-08 07:26:41 +0300

    * object: add ConfigException,EnumException,FactoryException,RegistryException.

---
+++ src/object/FactoryException.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Copyright (c) 2015 · Kerem Güneş
+ * Apache License 2.0 · http://github.com/froq/froq-common
+ */
+declare(strict_types=1);
+
+namespace froq\common\object;
+
+use froq\common\Exception;
+
+/**
+ * Factory Exception.
+ *
+ * @package froq\common\object
+ * @object  froq\common\object\FactoryException
+ * @author  Kerem Güneş
+ * @since   6.0
+ */
+class FactoryException extends Exception
+{}
