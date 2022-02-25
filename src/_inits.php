1546e751965767ea477452ee5e94de3125b3b632
Kerem Güneş <qeremy@gmail.com>
2022-02-25 04:00:30 +0300

    * _inits: move dump stuff into util.

--- src/_inits.php
+++ src/_inits.php
@@ -253,28 +253,3 @@ declare(strict_types=1);
         return array_prepend($array, ...$values);
     }
 }
-
-// Dirty debug (dump) tools.. :(
-{
-    function _ps($s) {
-        if (is_null($s)) return 'NULL';
-        if (is_bool($s)) return $s ? 'TRUE' : 'FALSE';
-        return preg_replace('~\[(.+?):.+?:(private|protected)\]~', '[\1:\2]', print_r($s, true));
-    }
-    function _pd($s) {
-        ob_start(); var_dump($s); $s = ob_get_clean();
-        return preg_replace('~\["?(.+?)"?(:(private|protected))?\]=>\s+~', '[\1\2] => ', _ps(trim($s)));
-    }
-    function pre($s, $e=0) {
-        echo "<pre>", _ps($s), "</pre>", "\n";
-        $e && exit;
-    }
-    function prs($s, $e=0) {
-        echo _ps($s), "\n";
-        $e && exit;
-    }
-    function prd($s, $e=0) {
-        echo _pd($s), "\n";
-        $e && exit;
-    }
-}
