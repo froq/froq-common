1cee33d0ab9c163ce450680eecff2c8526c5b90d
Kerem Güneş <1508306+okerem@users.noreply.github.com>
2022-12-02 21:07:53 +0300

    Make-ups.

--- src/object/ConfigException.php
+++ src/object/ConfigException.php
@@ -13,49 +13,21 @@ namespace froq\common\object;
  */
 class ConfigException extends \froq\common\Exception
 {
-    /**
-     * Create for absent dotenv file.
-     *
-     * @param  string $file
-     * @return static
-     */
     public static function forAbsentDotEnvFile(string $file): static
     {
         return new static('No .env file exists such %q', $file);
     }
 
-    /**
-     * Create for read dotenv file error.
-     *
-     * @param  string $file
-     * @return static
-     */
     public static function forReadDotEnvFileError(string $file): static
     {
         return new static('Cannot read .env file %q [error: @error]', $file, extract: true);
     }
 
-    /**
-     * Create for invalid dotenv entry.
-     *
-     * @param  string $entry
-     * @param  string $file
-     * @param  int    $line
-     * @return static
-     */
     public static function forInvalidDotEnvEntry(string $entry, string $file, int $line): static
     {
         return new static('Invalid .env entry %q at %s:%s', [$entry, $file, $line]);
     }
 
-    /**
-     * Create for duplicated dotenv entry.
-     *
-     * @param  string $entry
-     * @param  string $file
-     * @param  int    $line
-     * @return static
-     */
     public static function forDuplicatedDotEnvEntry(string $entry, string $file, int $line): static
     {
         return new static('Duplicated .env entry %q at %s:%s', [$entry, $file, $line]);
